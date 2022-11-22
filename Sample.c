961d28e3b80680f8ab014a5ac7dca1a6096508ba
214G5A0501 <111738579+214G5A0501@users.noreply.github.com>
2022-11-22 12:33:02 +0530

    akshaya Created Sample.c

---
+++ Sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: G.Akshaya
+   Roll No: 214G5A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
